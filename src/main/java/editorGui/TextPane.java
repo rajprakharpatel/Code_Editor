b35c935f6c627d66ca2383247e7deef2973ac8e8
rajprakhar <rajprakhar2000@gmail.com>
2021-03-13 00:12:18 +0530

    Implemented textPane,GTK theme and added tabs

---
+++ src/main/java/editorGui/TextPane.java
@@ -0,0 +1,36 @@
+package editorGui;
+
+import java.awt.Color;
+import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+
+@SuppressWarnings("serial")
+public class TextPane extends JTextPane {
+    public TextPane() {
+        // Place holder text
+        // SimpleAttributeSet attributeSet = new SimpleAttributeSet();
+        // StyleConstants.setBold(attributeSet, true);
+        // this.setCharacterAttributes(attributeSet, true);
+        // this.setText("Welcome");
+        // attributeSet = new SimpleAttributeSet();
+        // StyleConstants.setItalic(attributeSet, true);
+        // StyleConstants.setForeground(attributeSet, Color.red);
+        // this.setBackground(Color.getHSBColor(0, 2, 13));
+        // this.setForeground(Color.WHITE);
+        // this.setText("Here is example text");
+
+        // try {
+        // Document doc = this.getStyledDocument();
+        // doc.insertString(doc.getLength(), "To Java ", attributeSet);
+        //
+        // attributeSet = new SimpleAttributeSet();
+        // doc.insertString(doc.getLength(), "World", attributeSet);
+        // } catch (BadLocationException e) {
+        // System.out.println("Error ocurred");
+        // }
+    }
+
+}
