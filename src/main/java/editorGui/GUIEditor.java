f22b77f6183639f4818365f2e179901e6f6f3968
rajprakhar <rajprakhar2000@gmail.com>
2021-03-13 00:12:18 +0530

    Implemented textPane,GTK theme and added tabs

--- src/main/java/editorGui/GUIEditor.java
+++ src/main/java/editorGui/GUIEditor.java
@@ -4,36 +4,64 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.event.*;
 
 @SuppressWarnings("serial")
 public class GUIEditor extends JFrame implements ActionListener {
 
-    TextArea textArea;
+    TextPane textPane;
     JScrollPane scrollPane;
+    JPanel panel;
+    JTabbedPane tabPane;
 
     MenuBar menuBar;
 
     public GUIEditor() {
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+        } catch (Exception e) {
+            try {
+                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            } catch (Exception ev) {
+                //e.printStackTrace();
+                System.err.println("Look and feel not set.");
+            }
+        }
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500, 500);
         this.setTitle("Code Editor");
+        this.getContentPane().setBackground(Color.DARK_GRAY);
+        this.setSize(500, 500);
         this.setLocationRelativeTo(null);
         this.setLayout(new BorderLayout());
 
         // Creating text area
-        textArea = new TextArea();
+        textPane = new TextPane();
 
-        // Adding vertical scroll bar
-        scrollPane = new JScrollPane(textArea);
-        scrollPane.setPreferredSize(new Dimension(480, 480));
-        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        // Panel to hold textPane and scrollBars
+        panel = new JPanel(new BorderLayout());
+        panel.add(textPane);
+
+        // Adding vertical and horizontal scroll bar
+        scrollPane = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setPreferredSize(new Dimension(500, 500));
 
         // Setting up menu bar
         menuBar = new MenuBar();
 
-        this.setJMenuBar(menuBar);
-        this.add(scrollPane);
+        // Setting up tabbed pane
+        tabPane = new JTabbedPane();
+        tabPane.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                // TODO: implememnt highlight to call from here
+                // textPane.highlight();
+            }
+        });
+        tabPane.add(scrollPane);
 
+        this.setJMenuBar(menuBar);
+        this.add(tabPane);
         this.setVisible(true);
 
     }
